f2b18815b8e61ef380ad2b089c759bab31c3f5c5
FaabB <fabian.beck@haw-hamburg.de>
2016-10-25 16:25:02 +0200

    google rest call

--- src/main/java/routing/RoutenService.java
+++ src/main/java/routing/RoutenService.java
@@ -1,12 +1,39 @@
 package routing;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+import java.util.List;
+
 import static spark.Spark.*;
 
 /**
  * Created by FBeck on 25.10.2016.
  */
 public class RoutenService {
-    public static void main(String[] args){
+    OkHttpClient client = new OkHttpClient();
+
+    public static void main(String[] args) throws IOException {
         get("/hello", (req, res) -> "Hello World");
+
+        RoutenService routenService = new RoutenService();
+        System.out.println(routenService.run("c"));
+
+
+
+    }
+
+    String run(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url("https://maps.googleapis.com/maps/api/geocode/json?address=Hermannstal+97&key=AIzaSyDMvwHv9F7evXsKaDGdIhKNUyjsyviV4aU")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 }
--- src/main/java/routing/rest/call/AddressConversion.java
+++
@@ -1,7 +0,0 @@
-package routing.rest.call;
-
-/**
- * Created by FBeck on 25.10.2016.
- */
-public interface AddressConversion {
-}
